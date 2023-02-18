a9f4dce285068557dcf6adfa3f801cbec7cbd4af
KiteCaptain <incaptain703@gmail.com>
2023-02-18 21:34:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed("#typed", {
+    strings: ['Hello There', 'How are you?', 'Wanna see something cool?', 'Just scroll down?'],
+    typeSpeed: 35,
+    startDelay: 500,
+    backSpeed: 35,
+    loop: true,
+    // loopCount: 2,
+    showCursor: true
+
+});
